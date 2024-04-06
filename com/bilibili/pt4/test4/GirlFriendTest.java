f8405515fe53d2a59370cf7b0b44754c7c305d17
Rhapsody-Zy <151840465+Rhapsody-Zy@users.noreply.github.com>
2024-04-06 23:05:18 +0800

    Add files via upload

---
+++ com/bilibili/pt4/test4/GirlFriendTest.java
@@ -0,0 +1,8 @@
+package com.bilibili.pt4.test4;
+
+public class GirlFriendTest {
+    public static void main(String[] args) {
+        GirlFriend gf1 = new GirlFriend();
+        gf1.method();
+    }
+}
