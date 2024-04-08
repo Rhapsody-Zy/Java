35519d6b079a96a32fc9b2fa440962e4e7b6e3df
Rhapsody-Zy <151840465+Rhapsody-Zy@users.noreply.github.com>
2024-04-08 23:18:37 +0800

    Add files via upload

---
+++ com/bilibili/pt04/test02/GirlfriendTest.java
@@ -0,0 +1,15 @@
+package com.bilibili.pt04.test02;
+
+public class GirlfriendTest {
+    public static void main(String[] args) {
+        Girlfriend gf1 = new Girlfriend();
+        gf1.name = "熏";
+        gf1.age = 15;
+        gf1.gender = "小提琴手";
+
+        System.out.println(gf1.name);
+
+        gf1.sleep();
+
+    }
+}
