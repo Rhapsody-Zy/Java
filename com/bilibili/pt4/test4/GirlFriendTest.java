9ca0b2bbea803eb764c919d23a2e1c99e235756d
Rhapsody-Zy <151840465+Rhapsody-Zy@users.noreply.github.com>
2024-04-08 23:16:27 +0800

    Delete com/bilibili/pt4 directory

--- com/bilibili/pt4/test4/GirlFriendTest.java
+++
@@ -1,8 +0,0 @@
-package com.bilibili.pt4.test4;
-
-public class GirlFriendTest {
-    public static void main(String[] args) {
-        GirlFriend gf1 = new GirlFriend();
-        gf1.method();
-    }
-}
