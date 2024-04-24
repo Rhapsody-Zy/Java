01142bdce68a9e1add2090b3d92a222338076ee3
Rhapsody-Zy <151840465+Rhapsody-Zy@users.noreply.github.com>
2024-04-24 23:19:04 +0800

    Add files via upload

---
+++ com/bilibili/pt07/test07/lianxi/Test.java
@@ -0,0 +1,8 @@
+package com.bilibili.pt07.test07.lianxi;
+
+public class Test {
+    public static void main(String[] args) {
+        Lecturer l = new Lecturer("01","张三");
+        l.work();
+    }
+}
