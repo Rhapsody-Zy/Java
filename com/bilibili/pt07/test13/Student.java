a8a8bdc12c78e34709606801ae89f98c6d837ed5
Rhapsody-Zy <151840465+Rhapsody-Zy@users.noreply.github.com>
2024-04-27 23:49:39 +0800

    Add files via upload

---
+++ com/bilibili/pt07/test13/Student.java
@@ -0,0 +1,41 @@
+package com.bilibili.pt07.test13;
+
+public class Student {
+    private String name;
+    private int age;
+
+    //构造代码块
+    //1.写在成员位置的代码块
+    //2.作用：可以把多个构造方法中重复的代码抽取出来
+    //3.执行时机：我们在创建本类对象时会先执行构造代码块再执行构造方法
+
+    {
+        System.out.println("构造代码块");
+    }
+
+    public Student() {
+        //System.out.println("开始创建对象");
+    }
+
+    public Student(String name, int age) {
+        //System.out.println("开始创建对象");
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ com/bilibili/pt07/test13/StudentStatic.java
@@ -0,0 +1,38 @@
+package com.bilibili.pt07.test13;
+
+public class StudentStatic {
+    private String name;
+    private int age;
+
+    //随着类的加载而加载，并且只执行一次
+    static {
+        System.out.println("静态代码块执行了");
+    }
+
+
+    public StudentStatic() {
+        System.out.println("空参构造");
+    }
+
+    public StudentStatic(String name, int age) {
+        System.out.println("有参构造");
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
