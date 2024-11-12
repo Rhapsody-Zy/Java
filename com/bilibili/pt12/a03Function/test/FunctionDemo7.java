a54ffda5e39bb53b3a0f0700b83cf4f1409d116d
rhapsody <1972095633@qq.com>
2024-11-13 00:05:51 +0800

    Add files via upload

---
+++ com/bilibili/pt12/a03Function/test/FunctionDemo7.java
@@ -0,0 +1,17 @@
+package com.bilibili.pt12.a03Function.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FunctionDemo7 {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list,"张无忌,21","周芷若,20","赵敏,22","张强,20","张三丰,24","张良,35","王二麻子,37");
+
+        List<Student> newList = list.stream().map(Student::new).collect(Collectors.toList());
+        System.out.println(newList);
+    }
+}
