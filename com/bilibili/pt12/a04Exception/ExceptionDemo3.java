b03ea5036e1a3a922696aca1d53c04e44fbb5d39
rhapsody <1972095633@qq.com>
2024-11-13 23:24:00 +0800

    Add files via upload

---
+++ com/bilibili/pt12/a04Exception/ExceptionDemo3.java
@@ -0,0 +1,22 @@
+package com.bilibili.pt12.a04Exception;
+
+public class ExceptionDemo3 {
+    public static void main(String[] args) {
+        /*
+        try catch
+        好处：出现异常时可以让程序继续往下执行，不停止
+        */
+
+        int[] arr = {1,2,3,4,5};
+
+        try {
+            //可能出现异常的代码
+            System.out.println(arr[10]);
+        }catch (ArrayIndexOutOfBoundsException e) {
+            //如果出现了这个异常，将怎么做
+            System.out.println("索引越界");
+        }
+        System.out.println("继续执行");
+
+    }
+}
---
+++ com/bilibili/pt12/a04Exception/ExceptionDemo5.java
@@ -0,0 +1,33 @@
+package com.bilibili.pt12.a04Exception;
+
+public class ExceptionDemo5 {
+    public static void main(String[] args) {
+
+        //抛出异常
+
+        int[] arr = null;
+        int max = 0;
+        try {
+            max = getMax(arr);
+        } catch (NullPointerException e) {
+            System.out.println("空指针异常");;
+        }
+        System.out.println(max);
+    }
+
+    public static int getMax(int[] arr) {
+        //手动创建一个异常对象，并把这个对象交给方法的调用者处理
+        //此时方法结束，下面方法不会执行
+        /*if (arr == null) {
+            throw new NullPointerException();
+        }*/
+
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
