1c1e3fb52034abb224d4239703a7f73cb82d4ca3
rhapsody <1972095633@qq.com>
2024-11-27 22:49:06 +0800

    Add files via upload

--- com/bilibili/pt13/Test/Test08.java
+++ com/bilibili/pt13/Test/Test08.java
@@ -1,4 +1,17 @@
 package com.bilibili.pt13.Test;
 
+import java.io.*;
+
 public class Test08 {
+    public static void main(String[] args) throws IOException {
+
+        //利用字节流读取文件中的数据，每次读一整行，而且不能出现乱码
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("W:\\Desktop\\demo\\e.txt")));
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+    }
 }
