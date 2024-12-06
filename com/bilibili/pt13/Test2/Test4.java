9181a5ce752b6522cae30014502c00b54c52b662
rhapsody <1972095633@qq.com>
2024-12-06 21:49:59 +0800

    Add files via upload

---
+++ com/bilibili/pt13/Test2/Test4.java
@@ -0,0 +1,64 @@
+package com.bilibili.pt13.Test2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Test4 {
+    public static void main(String[] args) throws IOException {
+        String src = "W:\\Desktop\\demo\\name.txt";
+        String backup = "W:\\Desktop\\demo\\backup.txt";
+
+        //第一次点名读取原始文件
+        ArrayList<String> list = readFile(src);
+        //第一边点完名后原始文件为空时
+        if (list.size() == 0) {
+            //从备份文件中读取信息
+            list = readFile(backup);
+            //将读取到的信息存到原始文件中
+            writeFile(src,list,false);
+            //删除备份文件
+            new File(backup).delete();
+        }
+        //打乱list中的顺序
+        Collections.shuffle(list);
+        //将list中第一个元素抛出
+        String stuInfo = list.remove(0);
+        System.out.println(stuInfo);
+        //将抛出后的list覆盖写入原始文件中
+        writeFile(src,list,false);
+        //将抛出的数据写到备份文件中
+        writeFile(backup,stuInfo,true);
+
+
+    }
+
+    public static void writeFile(String pathFile, String name, boolean isAppend) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(pathFile,isAppend));
+        bw.write(name);
+        bw.newLine();
+        bw.close();
+    }
+
+    public static void writeFile(String pathFile, ArrayList<String> list, boolean isAppend) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(pathFile,isAppend));
+        for (String s : list) {
+            bw.write(s);
+            bw.newLine();
+        }
+        bw.close();
+    }
+
+    public static ArrayList<String> readFile(String pathFile) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(pathFile));
+        String str;
+        while ((str = br.readLine()) != null) {
+            list.add(str);
+        }
+        br.close();
+        return list;
+
+    }
+}
---
+++ com/bilibili/pt13/Test2/Test5.java
@@ -0,0 +1,60 @@
+package com.bilibili.pt13.Test2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Test5 {
+    public static void main(String[] args) throws IOException {
+
+        //带权重的随机点名器
+
+        ArrayList<Student> list = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("W:\\Desktop\\demo\\name.txt"));
+        String str;
+        while ((str = br.readLine()) != null) {
+            String[] arr = str.split("-");
+            Student stu = new Student(arr[0],arr[1],Integer.parseInt(arr[2]),Double.parseDouble(arr[3]));
+            list.add(stu);
+        }
+        br.close();
+
+        //计算权重
+        double weight = 0;
+        for (Student stu : list) {
+            weight += stu.getWeight();
+        }
+
+        //获取每个学生的权重，将权重写入一个arr中
+        double[] arr = new double[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i).getWeight() / weight;
+        }
+
+        //计算每个人的权重占比范围
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i] + arr[i - 1];
+        }
+
+        //随机抽取
+        double number = Math.random();
+        //判断 - 二分查找法
+        //返回值为   -插入点 - 1
+        int index = -Arrays.binarySearch(arr, number) - 1;
+        Student stu = list.get(index);
+        System.out.println(stu);
+
+        //修改档期那学生的权重
+        double w = stu.getWeight() / 2;
+        stu.setWeight(w);
+
+        //将新的权重信息写入文档中
+        BufferedWriter bw = new BufferedWriter(new FileWriter("W:\\Desktop\\demo\\name.txt"));
+        for (Student stu1 : list) {
+            bw.write(stu1.toString());
+            bw.newLine();
+        }
+        bw.close();
+
+    }
+}
