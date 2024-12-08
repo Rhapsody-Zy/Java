b47744a1fbc3390c694f0600240ef30b6f7773e7
rhapsody <1972095633@qq.com>
2024-12-08 22:03:43 +0800

    Add files via upload

--- com/puzzlegame/test/demo01/Test3.java
+++
@@ -1,9 +0,0 @@
-package com.bilibili.test.demo01;
-
-import com.bilibili.test.demo01.MyAction;
-
-public class Test3 {
-    public static void main(String[] args) {
-        new MyAction();
-    }
-}
