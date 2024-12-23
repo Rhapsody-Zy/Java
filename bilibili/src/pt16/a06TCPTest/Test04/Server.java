928db2e625ba458880fe6481c93358e1eba4b1ad
rhapsody <1972095633@qq.com>
2024-12-23 22:27:49 +0800

    Add files via upload

--- bilibili/src/pt16/a06TCPTest/Test04/Server.java
+++ bilibili/src/pt16/a06TCPTest/Test04/Server.java
@@ -4,14 +4,28 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.UUID;
+import java.util.concurrent.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
 
+        //创建线程池对象
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(
+                3,      //核心线程数量
+                16,                 //线程池大小
+                60,                 //空闲时间
+                TimeUnit.SECONDS,       //空闲时间（单位）
+                new ArrayBlockingQueue<>(2),    //队列
+                Executors.defaultThreadFactory(),       //线程工厂，让线程如何创建线程对象
+                new ThreadPoolExecutor.AbortPolicy()    //阻塞队列
+        );
+
         ServerSocket ss = new ServerSocket(10000);
         while (true) {
             Socket socket = ss.accept();
-            new Thread(new MyRunnable(socket)).start();
+            //new Thread(new MyRunnable(socket)).start();
+            pool.submit(new MyRunnable(socket));
+
         }
 
 
