c7826ff008172ffc099cbcb268ce4474da4b14a1
Rhapsody-Zy <1972095633@qq.com>
2025-01-16 20:25:37 +0800

    Add files via upload

--- maven-demo/src/main/java/com/bilibili/mapper/BrandMapper.java
+++ maven-demo/src/main/java/com/bilibili/mapper/BrandMapper.java
@@ -2,10 +2,37 @@ package com.bilibili.mapper;
 
 import com.bilibili.pojo.Brand;
 import com.bilibili.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
     List<Brand> selectAll();
 
+    Brand selectById(int id);
+
+    /*条件查询
+        1.散装参数
+        2.对象参数
+        3.map集合参数
+        */
+
+    //List<Brand> selectByCondition(@Param("status") int status, @Param("companyName") String companyName, @Param("brandName") String brandName);
+    List<Brand> selectByCondition(Map map);
+
+    /*单条件查询*/
+    List<Brand> selectByConditionSingle(Brand brand);
+
+    /*添加数据*/
+    void add(Brand brand);
+
+    /*修改数据*/
+    int update(Brand brand);
+
+    /*删除数据*/
+    void deleteById(int id);
+
+    /*批量删除*/
+    void deleteByIds(@Param("ids") int[] ids);
 }
--- maven-demo/src/test/java/com/bilibili/test/MyBatisTest.java
+++ maven-demo/src/test/java/com/bilibili/test/MyBatisTest.java
@@ -10,7 +10,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MyBatisTest {
 
@@ -23,9 +25,162 @@ public class MyBatisTest {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
 
-        List<Brand> brands = brandMapper.selectAll();
+        //List<Brand> brands = brandMapper.selectAll();
+        //System.out.println(brands);
+
+        Brand brand = brandMapper.selectById(1);
+        System.out.println(brand);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void selectByConditions() throws IOException {
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        Map map = new HashMap();
+        map.put("status",status);
+        map.put("companyName",companyName);
+        //map.put("brandName",brandName);
+
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        List<Brand> brands = brandMapper.selectByCondition(map);
         System.out.println(brands);
 
+        sqlSession.close();
+    }
+    @Test
+    public void selectByConditionSingle() throws IOException {
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        Brand brand = new Brand();
+        brand.setStatus(status);
+        //brand.setCompanyName(companyName);
+        //brand.setBrandName(brandName);
+
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        List<Brand> brands = brandMapper.selectByConditionSingle(brand);
+        System.out.println(brands);
+
+        sqlSession.close();
+
+
+    }
+
+    @Test
+    public void add() throws IOException {
+        String companyName = "特斯拉";
+        String brandName = "特斯拉";
+        String description = "牛";
+        int ordered = 100;
+        int status = 1;
+
+        Brand brand = new Brand();
+        brand.setCompanyName(companyName);
+        brand.setBrandName(brandName);
+        brand.setDescription(description);
+        brand.setOrdered(ordered);
+        brand.setStatus(status);
+
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        brandMapper.add(brand);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    public void update() throws IOException {
+        String companyName = "特斯拉";
+        String brandName = "特斯拉";
+        String description = "科技";
+        int ordered = 100;
+        int status = 1;
+        int id = 4;
+
+        Brand brand = new Brand();
+        brand.setCompanyName(companyName);
+        brand.setBrandName(brandName);
+        brand.setDescription(description);
+        brand.setOrdered(ordered);
+        brand.setStatus(status);
+        brand.setId(id);
+
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        int count = brandMapper.update(brand);
+        System.out.println(count);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    public void deleteById() throws IOException {
+        int id = 5;
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        brandMapper.deleteById(id);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteByIds() throws IOException {
+        int[] ids = {5,6,7,8};
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        brandMapper.deleteByIds(ids);
+
+        sqlSession.commit();
         sqlSession.close();
     }
 }
